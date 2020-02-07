b747ea244d4a821ee36181634e1caa45ac7db418
andresponton21 <andresponton21@yahoo.com>
2020-02-06 20:34:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const Web3 = require('web3'); 
+var Tx = require('ethereumjs-tx');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+web3.eth.defaultAccount = web3.eth.accounts[0];
+
+var TestContract = new web3.eth.Contract([
+	{
+		"inputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "withdrawAll",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+],'0x63cBf3B2119C68f130d3E9d593a3d5927C59c41C');
+//var Test = TestContract.at('0x63cBf3B2119C68f130d3E9d593a3d5927C59c41C');
+var account = "0xA20B75d1B7F1b2bC22A3c0D2e61Cdb3daCbedB3E";
+
+var privateKey = new Buffer("bc5909a35c6d282703db296fc8f9fbe3ee5b1c74bcfa7c1e4bc4dd8c6fbed83c");
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+var rawTx = {
+  nonce: '0x00',
+  gasPrice: '0x09184e72a000',
+  gasLimit: '0x2710',
+  to: '0x0000000000000000000000000000000000000000',
+  value: '0x00',
+  data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
+}
+
+var tx = new Tx(rawTx);
+tx.sign(privateKey);
+
+var serializedTx = tx.serialize();
+
+web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+.on('receipt', console.log);
